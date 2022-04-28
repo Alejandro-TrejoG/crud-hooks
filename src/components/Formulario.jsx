bc916dfd29a3128ca6b0cda566bae7c722572a61
Alejandro Trejo Godinez <alejandro.trejog.15h@gmail.com>
2022-04-28 13:59:48 -0500

    Funcion editar tareas añadida

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -4,8 +4,10 @@ import Swal from 'sweetalert2'
 // import ListaTareas from './ListaTareas'
 
 const Formulario = () => {
+    const [id, setId] = React.useState("")
     const [tarea, setTarea] = React.useState("")
     const [tareas, setTareas] = React.useState([])
+    const [editar, setEditar] = React.useState(false)
 
 
     const agregarTarea = e => {
@@ -40,7 +42,7 @@ const Formulario = () => {
                 Swal.fire(
                     '¡Eliminado!',
                     'La tarea se ha borrado',
-                    'Entendido'
+                    'success'
                 )
             }
         })
@@ -49,27 +51,75 @@ const Formulario = () => {
         // setTareas([...tareas])
     }
 
+    const editarTarea = tarea => {
+        // // console.table(tarea)
+        setEditar(true)
+        setTarea(tarea.task)
+        setId(tarea.id)
+        // // console.log(tarea.id)
+    }
+
+    const guardarCambio = () => {
+        tareas.forEach(item => {
+            if (item.id == id) {
+                if (tarea !== "") {
+                    Swal.fire({
+                        title: '¿Estás seguro de guardar el cambio?',
+                        // text: "You won't be able to revert this!",
+                        icon: 'question',
+                        showCancelButton: true,
+                        confirmButtonColor: '#3085d6',
+                        cancelButtonColor: '#d33',
+                        cancelButtonText: "Cancelar",
+                        confirmButtonText: '¡Si!'
+                    }).then((result) => {
+
+                        if (result.isConfirmed) {
+
+                            item.task = tarea
+                            setTarea("")
+                            setEditar(false)
+
+                            Swal.fire(
+                                '¡Editado!',
+                                'La tarea se ha editado',
+                                'success'
+                            )
+                        } else {
+                            setTarea("")
+                            setEditar(false)
+                        }
+                    })
+                }
+
+            }
+        })
+    }
+
     return (
         <div className='col-12 contenedor-main main'>
             <div className="col-sm-12 col-md-7 col-lg-7 col-xl-7">
                 <h2>ListaTareas</h2>
                 <div className='contenedor-tarea  p-2 row'>
                     {
-                        tareas.map((tarea, index) => {
-                            console.log(index)
-                            return <div className=' div-tarea row col-12 mb-3 border border-secondary pt-2 pb-5' id={index} key={tarea.id}>
-                                <p className='col'>{tarea.task}</p>
-                                <button type='button' className='btn btn-warning mr-1 col-1'>Editar</button>
-                                <button type='button' className='btn btn-danger col-2' onClick={() => eliminarTarea(index)}>Eliminar</button>
-                            </div>
-                        })
+                        tareas.length == 0 ? ("No hay tareas por mostrar") :
+                            (tareas.map((tarea, index) => {
+                                console.log(index)
+                                return <div className=' div-tarea row col-12 mb-3 border border-secondary pt-2 pb-5' id={index} key={tarea.id}>
+                                    <p className='col'>{tarea.task}</p>
+                                    <button type='button' className='btn btn-warning mr-1 col-1' onClick={() => editarTarea(tarea)}>Editar</button>
+                                    <button type='button' className='btn btn-danger col-2' onClick={() => eliminarTarea(index)}>Eliminar</button>
+                                </div>
+                            }))
                     }
                 </div>
             </div>
             <form action="" onSubmit={agregarTarea} className='formulario col-sm-12 col-md-4 col-lg-4 col-xl-4'>
-                <h2>Formulario</h2>
+                <h2>{editar ? "Editar tarea" : "Agregar tarea"}</h2>
                 <input type="text" placeholder='Ingrese la tarea' className='col-12 p-2' onChange={e => setTarea(e.target.value)} value={tarea} required />
-                <button type='submit' className='btn btn-primary col-12 mt-3 p-2'> Agregar</button>
+                {editar ? (<button type='button' className='btn btn-primary col-12 mt-3 p-2' onClick={() => guardarCambio()}>Guardar cambios</button>)
+                    : (<button type='submit' className='btn btn-dark col-12 mt-3 p-2'>Agregar</button>)}
+
             </form>
         </div>
     )
